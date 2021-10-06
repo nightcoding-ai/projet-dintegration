238cb44cb0e5838b9ddc8d17c30cdaed0e5abd57
Simon Kinet <64420845+Nepsylon@users.noreply.github.com>
2021-10-06 16:28:04 +0200

    Navbar
    Implémentation de la navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar, Nav } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg="dark" variant="dark" sticky="top" expand="lg">
+            <Navbar.Brand>
+                <img src={logo} width="80%" alt="logo" className="mx-2" />
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                <Nav className="me-auto mx-5">
+                    <Nav.Link className="Link px-3" href="Accueil">Accueil</Nav.Link>
+                    <Nav.Link className="Link px-3" href="Marché">Marché</Nav.Link>
+                    <Nav.Link className="Link px-3" href="Espace_Com">Espace commerçant</Nav.Link>
+                    <Nav.Link className="Link px-3" href="Scanner">Scanner</Nav.Link>
+                    <Nav.Link className="Link px-3" href="Contact">Nous contacter</Nav.Link>
+                </Nav>
+                <Nav className="mx-5">
+                     <Nav.Link className="Link  px-3" href="Panier">Panier</Nav.Link>
+                     <Nav.Link className="Link  px-3" href="Connexion">Connexion</Nav.Link>
+                </Nav>
+
+            </Navbar.Collapse>
+        </Navbar>
+        <div className="content">
+            On est sur bangoo!
+        </div>
     </div>
   );
 }
