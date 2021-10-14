17e6f2629de4cdf1e453cb7d989e267d2085c28e
Simon Kinet <64420845+Nepsylon@users.noreply.github.com>
2021-10-14 10:42:28 +0200

    Navbar en cours de cosntruction

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
     render() {
         return (
             <div className='home'>
-                <div className='hello'>Bienvenue sur <img src={logo} alt='logo'></img> !</div>
+                <div className='hello'>Bienvenue sur <img src={logo} alt='logo'></img> </div>
             </div>
         )
     }
