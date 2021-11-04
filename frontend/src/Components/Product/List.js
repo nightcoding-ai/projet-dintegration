4b88e79aa3b9e2d1b0cf1eff427c80829d71cddb
Hotmailnico <63592532+Hotmailnico@users.noreply.github.com>
2021-11-04 16:19:53 +0100

    listCommerce ok get, reste le delete a faire

---
+++ frontend/src/Components/Product/List.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import './Product.css';
+import Product from "./Product";
+import { Button } from 'react-bootstrap';
+
+class ListCommerce extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          show: false,
+          items: [],
+          isLoaded: false
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/products/')
+            .then((result) => {
+                this.setState({ 
+                    isLoaded: true,
+                    items: result.data
+                })
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
+
+    deleteProduct() {
+
+    }
+
+    render() {
+        const { items } = this.state;
+        if (!this.state.isLoaded) {
+            return <div>Chargement ... </div>;
+        } else {
+            return(
+                <div>
+                    <div id="product">
+                        <Product></Product>
+                    </div>
+                    <table class="table">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Stock</th>
+                                <th scope="col">Price</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tableBody">
+                        {items.map((product) => (
+                            <tr>
+                                <td id="list">{product.name}</td>
+                                <td id="list">{product.stock}</td>
+                                <td id="list">{product.price} <Button id="deleteButton" onClick={this.deleteProduct}>Delete</Button></td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            )
+        }
+    }
+}
+
+export default ListCommerce;
