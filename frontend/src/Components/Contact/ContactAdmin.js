ad8edae83f70888128267bfaa3d50a438e472901
Nicolas <martounicolas@gmail.com>
2021-12-01 11:46:01 +0100

    Correction du state

--- frontend/src/Components/Contact/ContactAdmin.js
+++ frontend/src/Components/Contact/ContactAdmin.js
@@ -13,12 +13,17 @@ class ContactAdmin extends Component {
         };
     }
 
+<<<<<<< refs/remotes/origin/develop
     show = () => {
+=======
+    show() {
+>>>>>>> Correction du state
         this.setState((state) => {
             return {hidden: this.state.hidden ? false : true}
         });
     }
 
+<<<<<<< refs/remotes/origin/develop
     verifyContact() {
         
     }
@@ -137,6 +142,63 @@ class ContactAdmin extends Component {
                             </tbody>
                         </table>
                     </div>
+=======
+    render() {
+        return (
+            <div>
+                <div id="openTable">
+                    <h3>Requêtes ouvertes</h3>
+                    <table className="table 5p">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Objet</th>
+                                <th scope="col">Statut</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td><span id="textObjet">Mark</span><button id="btnShow" className="btn btn-dark" onClick={this.show}>Plus..</button></td>
+                                <td id="statut">
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected id="openSelect" value="open">Ouvert</option>
+                                        <option id="closeSelect" value="close">Fermé</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr className={this.state.hidden ? "d-none" : "d-default"} id="table1">
+                                <td></td>
+                                <td><textarea id="text1"></textarea><button className="btn btn-primary" id="btn">Envoyer</button></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div id="closeTable">
+                <h3>Requêtes fermées</h3>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Objet</th>
+                                <th scope="col">Statut</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td id="statut">
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option id="openSelect" value="open">Ouvert</option>
+                                        <option selected id="closeSelect" value="close">Fermé</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+>>>>>>> Correction du state
                 </div>
             )
         }
