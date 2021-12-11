4f1e9c9824c775b579d34f02d361d009f3f861ac
Christopher Fauconnier <christopherfauconnier@gmail.com>
2021-12-11 15:14:41 +0100

    fix contact rebase

--- backend/controllers/contact.controller.js
+++ backend/controllers/contact.controller.js
@@ -1,8 +1,6 @@
 const ContactModel = require('../models/Contact')
 
 const contactCtrl = {
-<<<<<<< refs/remotes/origin/develop
-<<<<<<< refs/remotes/origin/develop
     getOpenRequests : async(req, res, next) =>{
         try {
             const requests = await ContactModel.find({status: true})
@@ -15,12 +13,18 @@ const contactCtrl = {
     getClosedRequests : async(req, res, next) =>{
         try {
             const requests = await ContactModel.find({status: false})
-=======
+        }catch(err){
+            return res.status(500).json({msg : err.message})
+        }
+    },
     getRequests : async(req, res, next) =>{
         try {
             const requests = await ContactModel.find()
->>>>>>> Avancement contact user front + back mais pas fini
-=======
+        } catch(err){
+            return res.status(500).json({msg : err.message})
+        }
+    },
+
     getOpenRequests : async(req, res, next) =>{
         try {
             const requests = await ContactModel.find({status: true})
@@ -33,7 +37,6 @@ const contactCtrl = {
     getClosedRequests : async(req, res, next) =>{
         try {
             const requests = await ContactModel.find({status: false})
->>>>>>> Contact user fini + contact admin get ok, reste a terminer le reste
             console.log(requests)
             res.json(requests)
         } catch(err){
@@ -42,29 +45,12 @@ const contactCtrl = {
     },
     createRequest : async(req, res,next) =>{
         try {
-<<<<<<< refs/remotes/origin/develop
-<<<<<<< refs/remotes/origin/develop
             const {mail, subject, message, status, response} = req.body;
 
             const newRequest = new ContactModel({
                 mail, subject, message, status, response
             })
             await newRequest.save()
-=======
-            const {mail, object, message, status, response} = req.body;
-=======
-            const {mail, subject, message, status, response} = req.body;
->>>>>>> Contact user fini + contact admin get ok, reste a terminer le reste
-
-            const newRequest = new ContactModel({
-                mail, subject, message, status, response
-            })
-<<<<<<< refs/remotes/origin/develop
-            await newProduct.save()
->>>>>>> Avancement contact user front + back mais pas fini
-=======
-            await newRequest.save()
->>>>>>> Contact user fini + contact admin get ok, reste a terminer le reste
             res.json({msg: "Created a request"})
             
         } catch(err) {
@@ -73,8 +59,6 @@ const contactCtrl = {
     },
     updateRequest : async(req,res, next) =>{
         try {
-<<<<<<< refs/remotes/origin/develop
-<<<<<<< refs/remotes/origin/develop
             const {mail, subject, message, status, response} = req.body;
 
             await ContactModel.findOneAndUpdate({_id : req.params.id}, {
@@ -92,26 +76,10 @@ const contactCtrl = {
 
             await ContactModel.findOneAndUpdate({_id : req.params.id}, {
                 status: status
-=======
-            const {mail, object, message, status, response} = req.body;
-
-            await ProductModel.findOneAndUpdate({_id : req.params.id}, {
-                mail, object, message, status, response
->>>>>>> Avancement contact user front + back mais pas fini
-=======
-            const {mail, subject, message, status, response} = req.body;
-
-            await ContactModel.findOneAndUpdate({_id : req.params.id}, {
-                mail, subject, message, status, response
->>>>>>> Contact user fini + contact admin get ok, reste a terminer le reste
             })
-
+            
             res.json({msg: "Updated a request"})
         }catch(err){
-<<<<<<< refs/remotes/origin/develop
-            console.log(err);
-=======
->>>>>>> Avancement contact user front + back mais pas fini
             return res.status(500).json({msg: err.message})
         }
     },
