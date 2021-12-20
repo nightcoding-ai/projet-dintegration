0745426aafa2deea24e61fb4b48c4eec8ffb21a1
Simon Kinet <64420845+Nepsylon@users.noreply.github.com>
2021-12-20 20:19:20 +0100

    Debugging stripe payment

--- frontend/src/Components/Cart/CheckoutForm.js
+++ frontend/src/Components/Cart/CheckoutForm.js
@@ -2,24 +2,34 @@ import React from 'react';
 import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
 import CardSection from './CardSection';
 import './Cart.css';
+import axios from 'axios';
+
 
 function CheckoutForm() {
   const stripe = useStripe();
   const elements = useElements();
 
+  const state = {
+    token:'',
+    name:'',
+    address:'',
+    cardHolder:'',
+    cardNumber:'',
+    expiMonth:'',
+    expiYear:'',
+    CVC:'',
+    items:[],
+    totalPrice : ''}
+
    function stripeTokenHandler (token) {
       const paymentData = {token: token.id};
-      console.log(paymentData)
+      state.token = paymentData.token;
    }
-
   const handleSubmit = async (event) => {
-    // We don't want to let default form submission happen here,
-    // which would refresh the page.
+
     event.preventDefault();
 
     if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make  sure to disable form submission until Stripe.js has loaded.
       return;
     }
      const payload = await stripe.createPaymentMethod({
@@ -27,10 +37,11 @@ function CheckoutForm() {
       card: elements.getElement(CardElement),
       billing_details: {
         name: event.target.name.value,
-        email: event.target.email.value
-      }
+        email: event.target.email.value,
+      },
 
     });
+
     console.log(payload)
     const card = elements.getElement(CardElement);
     const result = await stripe.createToken(card);
@@ -43,11 +54,21 @@ function CheckoutForm() {
       // This function does not exist yet; we will define it in the next step.
       stripeTokenHandler(result.token);
     }
+    state.name = payload.paymentMethod.billing_details.name
+    state.email = payload.paymentMethod.billing_details.email
+    console.log(state.name, state.email)
+    const order = {name: state.name, email: state.email, token: state.token}
+    console.log(order)
+    axios.post('http://localhost:5000/api/cart/checkout', order)
+      .then(() => console.log('Order posted'))
+      .catch(err => {});
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardSection />
+
+    <form onSubmit={handleSubmit} className="checkout-form" >
+      <CardSection
+      />
     </form>
   );
 }
