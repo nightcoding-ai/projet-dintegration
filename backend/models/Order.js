aa188c014a1387723277f7e2a04ba2e9d1cdad84
Brogniet <geoffrey.brogniet@gmail.com>
2021-12-21 16:52:04 +0100

    Ajout date
    Ajout du entrée Date pour avoir une date de la commande. Elle se fait automatiquement lors du post

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     cart: {type: Object, required: true},
     name: {type: String, required: true},
-    paymentId: {type: String, required: true}
+    paymentId: {type: String, required: true},
+    date : { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Order', schema);
